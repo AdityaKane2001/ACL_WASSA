1df02c7391fbef9c628d64f9693bc34eb96720c6
Aditya Kane <adityakane1@gmail.com>
2022-02-11 16:57:51 +0530

    Revert "Added baseline dataloader for bert"
    This reverts commit 950aa549f12a7714e4102132de34066240def11c.

--- utils.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def get_file_to_df(filepath):
-    if filepath.endswith(".tsv"):
-        return pd.read_csv(filepath, sep="\t")
-    elif filepath.endswith(".csv"):
-        return pd.read_csv(filepath)
