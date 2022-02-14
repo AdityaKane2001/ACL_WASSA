9e198b8cbd29433a39a25478a44b3ceb11651414
Aditya Kane <adityakane1@gmail.com>
2022-02-14 12:27:26 +0530

    Debugging...

--- train.py
+++ train.py
@@ -4,6 +4,10 @@ from config import get_config, get_static_config
 from utils import *
 from model import *
 
+import numpy as np
+
+from sklearn.metrics import f1_score
+
 import torch
 
 cfg = get_config(
@@ -48,41 +52,9 @@ def accuracy(true, pred):
     return float(100 * acc / len(true))
 
 
-def f1_loss(y_true: torch.Tensor, y_pred: torch.Tensor, is_training=False) -> torch.Tensor:
-    '''Calculate F1 score. Can work with gpu tensors
-    
-    The original implmentation is written by Michal Haltuf on Kaggle.
-    
-    Returns
-    -------
-    torch.Tensor
-        `ndim` == 1. 0 <= val <= 1
-    
-    Reference
-    ---------
-    - https://www.kaggle.com/rejpalcz/best-loss-function-for-f1-score-metric
-    - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html#sklearn.metrics.f1_score
-    - https://discuss.pytorch.org/t/calculating-precision-recall-and-f1-score-in-case-of-multi-label-classification/28265/6
-    
-    '''
-    assert y_true.ndim == 1
-    assert y_pred.ndim == 1 or y_pred.ndim == 2
-
-    if y_pred.ndim == 2:
-        y_pred = y_pred.argmax(dim=1)
-
-    tp = (y_true * y_pred).sum().to(torch.float32)
-    tn = ((1 - y_true) * (1 - y_pred)).sum().to(torch.float32)
-    fp = ((1 - y_true) * y_pred).sum().to(torch.float32)
-    fn = (y_true * (1 - y_pred)).sum().to(torch.float32)
-
-    epsilon = 1e-7
-
-    precision = tp / (tp + fp + epsilon)
-    recall = tp / (tp + fn + epsilon)
-
-    f1 = 2 * (precision*recall) / (precision + recall + epsilon)
-    f1.requires_grad = is_training
+def f1_loss(y_true: torch.Tensor, y_pred: torch.Tensor) -> torch.Tensor:
+    f1 = f1_score(y_true.detach().cpu().numpy(),
+                  np.argmax(y_pred.detach().cpu().numpy(), axis=-1))
     return f1
 
 device = torch.device(
