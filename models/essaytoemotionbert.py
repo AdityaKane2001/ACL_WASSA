594150d7863e89b40583e17c0a89a4613c4c6934
Aditya Kane <adityakane1@gmail.com>
2022-02-17 21:23:34 +0530

    Debugging...

--- models/essaytoemotionbert.py
+++ models/essaytoemotionbert.py
@@ -51,7 +51,7 @@ class EssayToEmotionBERT(nn.Module):
         emotion = self.emotion_lin(x)
         emotion = self.emotion_softmax(emotion)
 
-        return (emotion)
+        return (emotion, None)
 
     ### Utilities
     def push_all_to_device(self, device):
